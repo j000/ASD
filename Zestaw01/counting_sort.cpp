f38ca4be80b8f7a9ab2de70ae62770c9298697d6
j000 <jarymut+github@gmail.com>
2018-10-03 18:41:14 +0200

    [Zestaw01] Dodaj sortowanie przez zlicznie

---
+++ Zestaw01/counting_sort.cpp
@@ -0,0 +1,51 @@
+#include <ios>
+#include <iostream>
+#include <vector>
+
+template <class Iterator>
+inline void my_sort(Iterator begin, Iterator end)
+{
+	std::vector<int> histogram(*begin + 1);
+
+	for (auto i = begin; i != end; ++i) {
+		if (histogram.capacity() < (*i + 1)) { // comparing int and unsigned
+			auto old_size = histogram.capacity();
+			auto new_size = *i + 1;
+			histogram.reserve(new_size);
+			histogram.insert(histogram.end(), new_size - old_size, 0);
+		}
+		histogram[*i] += 1;
+	}
+
+	auto total = 0;
+	for (auto& e : histogram) {
+		auto old_count = e;
+		e = total;
+		total += old_count;
+	}
+
+	std::vector<int> output(end - begin);
+	for (auto i = begin; i != end; ++i) {
+		output[histogram[*i]] = *i;
+		histogram[*i] += 1;
+	}
+
+	for (auto i = begin, j = output.begin(); i != end; ++i, ++j)
+		*i = *j;
+}
+
+int main(int, char**)
+{
+	std::ios_base::sync_with_stdio(false);
+
+	std::vector<int> v;
+	int x;
+
+	while (std::cin >> x)
+		v.push_back(x);
+
+	my_sort(v.begin(), v.end());
+
+	for (auto e : v)
+		std::cout << e << std::endl;
+}
